d8c6b8ae57b39d0ea123f640308ac0c115dadd07
= <=>
2022-04-23 23:20:15 -0600

    Changes to Food API that were made

--- Food API/js/main.js
+++ Food API/js/main.js
@@ -23,10 +23,14 @@ function getMeal(){
       console.log(randMeal)
       document.querySelector('h2').innerText = data.meals[randMeal].strMeal
       document.querySelector('img').src = data.meals[randMeal].strMealThumb
-      document.querySelector('h3').innerText = data.meals[randMeal].strInstructions
+      document.querySelector('.instructions').innerText = data.meals[randMeal].strInstructions
       // Changing color from white to black so text can be seen
       document.querySelector('h2').style.color = "black"
-      document.querySelector('h3').style.color = "black"
+      document.querySelector('.instructions').style.color = "black"
+
+      // Getting each of the ingredients and measurements
+      let ingredientsNum = Object.keys(meals)
+      console.log(ingredientsNum)
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -44,6 +48,11 @@ function getMeal(){
       // Changing color from white to black so text can be seen
       document.querySelector('h2').style.color = "black"
       document.querySelector('h3').style.color = "white"
+
+      // Getting each of the ingredients and measurements
+      let ingredientsNum = Object.keys(data.meals[0])
+      console.log(ingredientsNum)
+      console.log(data.meals[0])
     })
     .catch(err => {
         console.log(`error ${err}`)
@@ -56,13 +65,42 @@ function getRandomMeal(){
     fetch(`https://themealdb.com/api/json/v1/1/random.php`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
+      // Initializing the list to put ingredients into
+      const ul = document.querySelector("ul")
+
+      // Remove everything so lists do not stack
+      ul.innerHTML = ''
+
+      ul.hidden = true
+
       console.log(data)
       document.querySelector('h2').innerText = data.meals[0].strMeal
       document.querySelector('img').src = data.meals[0].strMealThumb
-      document.querySelector('h3').innerText = data.meals[0].strInstructions
+      document.querySelector('.instructions').innerText = data.meals[0].strInstructions
       // Changing color from white to black so text can be seen
       document.querySelector('h2').style.color = "black"
-      document.querySelector('h3').style.color = "black"
+      document.querySelector('.instructions').style.color = "black"
+
+      // Getting each of the ingredients and measurements
+      let ingredientsNum = Object.keys(data.meals[0]).filter(str => str.indexOf("Measure") >= 0).length
+      let ingredientStr = ''
+      let ingredientMeas = ""
+      let ingredientActual = ""
+      let li = document.createElement("li")
+      for (i = 1; i <= ingredientsNum; i++) {
+        ingredientMeas = data.meals[0]["strMeasure" + i]
+        ingredientStr = data.meals[0]["strIngredient" + i]
+        if(ingredientStr != "") {
+          console.log(ingredientMeas)
+          ingredientActual = `${ingredientMeas} ${ingredientStr}`
+          li = document.createElement("li")
+          li.appendChild(document.createTextNode(ingredientActual))
+          ul.appendChild(li)
+        }
+        console.log(ingredientActual)
+        ul.hidden = false
+      }
+      
     })
     .catch(err => {
         console.log(`error ${err}`)
